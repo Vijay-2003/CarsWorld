cb1ad111b277acd5c0f61e14d7104b3fe76374d8
Vijay Digambar Kusekar <vijay.kusekar2003@gmail.com>
2026-08-03 13:25:45 +0530

    vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
-
+      <Analytics />
+      <SpeedInsights />
+    </BrowserRouter>
+  </React.StrictMode>
 )
